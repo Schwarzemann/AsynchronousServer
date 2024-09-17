2ee7b13b48e880344a345d94ac2b97b7f6eb9b6c
Schwarzemann <barantbsd@gmail.com>
2024-09-18 02:08:58 +0300

    Client side request/approval system with real time logging

--- win_iocp_client.cpp
+++ win_iocp_client.cpp
@@ -1,76 +1,55 @@
 #include <winsock2.h>
+#include <windows.h>
 #include <iostream>
-#include <cstring>
+#include <string>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 #define PORT 7777
 #define BUFFER_SIZE 1024
+#define SIGNATURE "signature" // This will be automated later but now this'll do
 
 int main() {
     WSADATA wsaData;
-    SOCKET clientSocket;
-    sockaddr_in serverAddr{};
-    char buffer[BUFFER_SIZE];
-    int recvLen;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "WSAStartup failed: " << WSAGetLastError() << "\n";
-        return 1;
-    }
-
-    // Create a socket
-    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (clientSocket == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed: " << WSAGetLastError() << "\n";
+        std::cerr << "Socket creation failed.\n";
         WSACleanup();
         return 1;
     }
 
-    // Set up the server address
+    sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Localhost
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Assuming server is running locally
 
-    // Connect to the server
     if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        std::cerr << "Connect failed: " << WSAGetLastError() << "\n";
+        std::cerr << "Connection to server failed.\n";
         closesocket(clientSocket);
         WSACleanup();
         return 1;
     }
 
-    std::cout << "Connected to server.\n";
+    std::cout << "Connected to the server. Press Enter to send a request...\n";
 
-    // Send a message to the server
-    std::string message = "succ";
-    if (send(clientSocket, message.c_str(), message.length(), 0) == SOCKET_ERROR) {
-        std::cerr << "Send failed: " << WSAGetLastError() << "\n";
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
+    while (true) {
+        std::cin.get();  // Wait for the user to press Enter
+
+        // Send request to server with the client's "signature"
+        std::string request = "REQUEST:" + std::string(SIGNATURE);
+        send(clientSocket, request.c_str(), request.size(), 0);
+
+        // Receive response from server
+        char buffer[BUFFER_SIZE] = { 0 };
+        int bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+        if (bytesReceived > 0) {
+            std::string response(buffer, bytesReceived);
+            std::cout << "Server Response: " << response << "\n";
+        }
     }
 
-    std::cout << "Message sent to server.\n";
-
-    // Receive a response from the server
-    recvLen = recv(clientSocket, buffer, BUFFER_SIZE, 0);
-    if (recvLen == SOCKET_ERROR) {
-        std::cerr << "Receive failed: " << WSAGetLastError() << "\n";
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    if (recvLen == 0) {
-        std::cout << "Server closed the connection.\n";
-    }
-    else {
-        std::cout << "Received from server: " << std::string(buffer, recvLen) << "\n";
-    }
-
-    // Cleanup
     closesocket(clientSocket);
     WSACleanup();
     return 0;
